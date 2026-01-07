fe4dca581f10e606a450ca7758893dc27efc3336
Bartek Dudek <bartlomiej.dudek94@gmail.com>
2026-01-07 18:01:29 +0100

    Initial commit

---
+++ src/main/java/pl/bartek/taskmanager/TaskController.java
@@ -0,0 +1,29 @@
+package pl.bartek.taskmanager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TaskController {
+
+    private final TaskRepository repository;
+
+    public TaskController(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("tasks", repository.findAll());
+        return "index";
+    }
+
+    @PostMapping("/add")
+    public String addTask(@RequestParam String title) {
+        repository.save(new Task(title, false));
+        return "redirect:/";
+    }
+}
